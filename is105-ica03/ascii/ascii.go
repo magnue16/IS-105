54105113986b168707e4e9ad3c98ea9d71d29fa5
osy16 <osmany16@student.uia.no>
2017-02-22 15:54:41 +0100

    sdsd

--- is105-ica03/ascii/ascii.go
+++ is105-ica03/ascii/ascii.go
@@ -43,8 +43,5 @@ func IterateOverASCIIStringLiteral(sl string) {
 // Gjelder oppgave 1b
 func GreetingASCII() {
 	a := "\x48\x65\x6C\x6C\x6F\x3A\x29"
-	fmt.Println(a)
-	for i := 0; i < len(a); i++ {
 	fmt.Printf("%s ", a)
-}
 }
