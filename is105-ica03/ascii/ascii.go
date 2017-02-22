bbd13dab3ca0fb0d205402553873393695e84fa8
osy16 <osmany16@student.uia.no>
2017-02-22 15:25:41 +0100

    sdds

--- is105-ica03/ascii/ascii.go
+++ is105-ica03/ascii/ascii.go
@@ -42,10 +42,9 @@ func IterateOverASCIIStringLiteral(sl string) {
 // som inneholder kun ASCII tegn (ikke utvidet ASCII).
 // Gjelder oppgave 1b
 func GreetingASCII() {
-	a := "hello :-)"
+	a := "\x48\x65\x6C\x6C\x6F\x3A\x29"
 	fmt.Println(a)
 	for i := 0; i < len(a); i++ {
-	fmt.Printf("%s", a)
+	fmt.Printf("%s ", a)
 }
-
 }
